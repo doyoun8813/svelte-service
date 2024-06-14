375ad091182376ff52b1ebde69c5e07c496bd52d
doyoun <doyoun8813@gmail.com>
2024-06-14 11:44:14 +0900

    Svelte 프로젝트를 위한 컴포넌트 배치

---
+++ frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  },
+  build: {
+    outDir: '../backend/src/main/resources/static',
+    emptyOutDir: true,
+  }
+})
