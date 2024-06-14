99ca6299291a1d1550981e0cdaf7fe012bd2a1a5
doyoun <doyoun8813@gmail.com>
2024-06-14 16:31:19 +0900

    인중 구현 - store

--- frontend/src/stores/index.js
+++ frontend/src/stores/index.js
@@ -1,13 +1,93 @@
-import { writeable, get } from 'svelte/store'
+import {writeable, get, writable} from 'svelte/store'
 import { getApi, putApi, delApi, postApi } from "../service/api.js";
 import { router } from 'tinro'
+import {options} from "axios";
 
 function setCurrentArticlesPage() {}
 function setArticles() {}
 function setLoadingArticle() {}
 function setArticleContent() {}
 function setComments() {}
-function setAuth() {}
+function setAuth() {
+    let initValues = {
+        id: '',
+        email: '',
+        Authorizatino: '',
+    }
+
+    const { subscribe, set, update } = writable({...initValues})
+
+    const refresh = async () => {
+        try{
+            const authenticationUser = await postApi({path: '/auth/refresh'})
+            set(authenticationUser)
+            isRefresh.set(true)
+        } catch (err){
+            auth.resetUserInfo()
+            isRefresh.set(false)
+        }
+    }
+    const resetUserInfo = () => set({...initValues})
+    const login = async (email, password) => {
+        try {
+            const Options = {
+                path: '/auth/login',
+                data: {
+                    email: email,
+                    pwd: password,
+                }
+            }
+
+            const result = await postApi(options);
+            set(result)
+            isRefresh.set(true)
+            router.goto('/articles')
+        } catch(err){
+            alert('오류가 발생했습니다. 로그인을 다시시도해주세요.');
+        }
+    }
+
+    const logout = async () => {
+        try{
+            const options = {
+                path: '/auth/logout'
+            }
+            await delApi(options);
+            set({...initValues})
+            isRefresh.set(false)
+            router.goto('/')
+        } catch (err){
+            alert('오류가 발생했습니다. 다시 시도해주세요.')
+        }
+    }
+
+    const register = async (email, pwd) => {
+        try{
+            const options = {
+                path: '/auth/register',
+                data: {
+                    email: email,
+                    pwd: pwd
+                }
+            }
+
+            await postApi(options);
+            alert('가입이 완료되었습니다.');
+            router.goto('/login')
+        } catch (err){
+            alert('오류가 발생했습니다. 다시 시도해 주세요.');
+        }
+    }
+
+    return {
+        subscribe,
+        refresh,
+        login,
+        logout,
+        resetUserInfo,
+        register,
+    }
+}
 function setArticlesMode() {}
 function setIsLogin() {}
 
@@ -19,3 +99,4 @@ export const comments = setComments()
 export const auth = setAuth()
 export const articlesMode = setArticlesMode()
 export const isLogin = setIsLogin()
+export const isRefresh = writable(false)
